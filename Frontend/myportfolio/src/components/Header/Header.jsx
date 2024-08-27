995d8be1bc76fbcba18f905c291dc21374df720d
Oumayma Hani <oumayma.hani278@gmail.com>
2024-08-27 15:02:46 +0200

    ADD header

--- Frontend/myportfolio/src/components/Header/Header.jsx
+++ Frontend/myportfolio/src/components/Header/Header.jsx
@@ -1,36 +1,77 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import React from "react";
 import Logo from "../../assets/images/logo.png";
 import "../Header/Header.scss";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <header className="header">
       <img className="header-logo" src={Logo} alt="Logo du site" />
-      <nav className="header_nav">
+
+      <nav className={`header_nav ${isOpen ? "active" : ""}`}>
         <ul>
           <li>
-            <NavLink to="/" className="header_nav__link ">
+            <NavLink
+              to="/"
+              onClick={closeMenu}
+              className={(header_nav__link) =>
+                header_nav__link.isActive ? "header_nav__link-active" : ""
+              }
+            >
               Présentation
             </NavLink>
           </li>
           <li>
-            <NavLink to="/compétences" className="header_nav__link ">
+            <NavLink
+              to="/compétences"
+              onClick={closeMenu}
+              className={(header_nav__link) =>
+                header_nav__link.isActive ? "header_nav__link-active" : ""
+              }
+            >
               Compétences
             </NavLink>
           </li>
           <li>
-            <NavLink to="/Réalisations" className="header_nav__link ">
+            <NavLink
+              to="/Réalisations"
+              onClick={closeMenu}
+              className={(header_nav__link) =>
+                header_nav__link.isActive ? "header_nav__link-active" : ""
+              }
+            >
               Réalisations
             </NavLink>
           </li>
           <li>
-            <NavLink to="/Contact" className="header_nav__link ">
+            <NavLink
+              to="/Contact"
+              onClick={closeMenu}
+              className={(header_nav__link) =>
+                header_nav__link.isActive ? "header_nav__link-active" : ""
+              }
+            >
               Contact
             </NavLink>
           </li>
         </ul>
       </nav>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </header>
   );
 }
