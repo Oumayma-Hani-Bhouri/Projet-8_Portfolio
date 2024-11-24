f1a68f5ef42c69b061cd8ec7c24c16f044ff1346
Oumayma Hani <oumayma.hani278@gmail.com>
2024-11-24 23:19:02 +0100

    add ScrolltoTop

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Notfoundpage from "./pages/error-page";
 import Projetdetails from "../src/pages/Projet/Projet";
+import ScrollToTop from "../src/components/ScrollToTop/ScrollToTop";
 
 const App = () => {
   return (
     <React.StrictMode>
       <BrowserRouter>
+        <ScrollToTop />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
